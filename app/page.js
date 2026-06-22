6a0f4967d77b0e3e35ad70c05c324c1b8ab0498d
Tommaso Cavalli <tommasocavalli.08@gmail.com>
2026-06-22 00:05:57 -0400

    middleware adjustments

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
-
 export default function Page() {
-    const country = headers().get("x-vercel-ip-country") || "IT";
-
-    redirect(country === "BR" ? "/pt" : "/it");
+    return <div>Home</div>;
 }
