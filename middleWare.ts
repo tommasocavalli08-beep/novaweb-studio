974481e55d833830210cc078f32d42d2a36f003b
Tommaso Cavalli <tommasocavalli.08@gmail.com>
2026-06-21 23:48:56 -0400

    middleware adjustments

--- middleWare.ts
+++ middleWare.ts
@@ -1,34 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-
-
-    // lascia passare asset e lingue già corrette
-    if (
-        pathname.startsWith("/_next") ||
-        pathname.startsWith("/it") ||
-        pathname.startsWith("/pt") ||
-        pathname.includes(".")
-    ) {
-        return NextResponse.next();
-    }
-
-    // SOLO root
-    if (pathname === "/") {
-        const country =
-            request.headers.get("x-vercel-ip-country") || "IT";
-
-        const url = request.nextUrl.clone();
-        url.pathname = country === "BR" ? "/pt" : "/it";
-
-        return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
+export function middleware() {
+    return NextResponse.redirect(new URL("/it", "https://example.com"));
 }
 
-// 👇 QUESTO è IL PUNTO CHIAVE
 export const config = {
-    matcher: ["/:path*"]
+    matcher: "/"
 };
