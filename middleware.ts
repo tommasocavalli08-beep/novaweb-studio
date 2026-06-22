dd7ea62165ade38dd03cf373a22d2aaadaa0aa64
Tommaso Cavalli <tommasocavalli.08@gmail.com>
2026-06-22 00:10:22 -0400

    middleware adjustments

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+/*import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
     const country =
@@ -17,4 +17,35 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     matcher: "/"
+}; */
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+
+    // evita loop e asset
+    if (
+        pathname.startsWith("/_next") ||
+        pathname.includes(".") ||
+        pathname.startsWith("/it") ||
+        pathname.startsWith("/pt")
+    ) {
+        return NextResponse.next();
+    }
+
+    if (pathname === "/") {
+        const country =
+            request.headers.get("x-vercel-ip-country") || "IT";
+
+        const url = request.nextUrl.clone();
+        url.pathname = country === "BR" ? "/pt" : "/it";
+
+        return NextResponse.redirect(url);
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: "/:path*"
 };
