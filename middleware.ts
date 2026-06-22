d80675ae62476fd131f8b46cb8c704e87df7bcea
Tommaso Cavalli <tommasocavalli.08@gmail.com>
2026-06-22 00:39:32 -0400

    middleware adjustments

--- middleware.ts
+++ middleware.ts
@@ -1,51 +1,22 @@
-/*import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-    const country =
-        request.headers.get("x-vercel-ip-country") || "IT";
-
-    const { pathname } = request.nextUrl;
-
-    if (pathname === "/") {
-        const url = request.nextUrl.clone();
-        url.pathname = country === "BR" ? "/pt" : "/it";
-        return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
-}
-
-export const config = {
-    matcher: "/"
-}; */
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
-    // evita loop e asset
-    if (
-        pathname.startsWith("/_next") ||
-        pathname.includes(".") ||
-        pathname.startsWith("/it") ||
-        pathname.startsWith("/pt")
-    ) {
+    // SOLO homepage
+    if (pathname !== "/") {
         return NextResponse.next();
     }
 
-    if (pathname === "/") {
-        const country =
-            request.headers.get("x-vercel-ip-country") || "IT";
+    const country =
+        request.headers.get("x-vercel-ip-country") || "IT";
 
-        const url = request.nextUrl.clone();
-        url.pathname = country === "BR" ? "/pt" : "/it";
+    const url = request.nextUrl.clone();
+    url.pathname = country === "BR" ? "/pt" : "/it";
 
-        return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
+    return NextResponse.redirect(url);
 }
 
 export const config = {
-    matcher: "/:path*"
+    matcher: "/"
 };
